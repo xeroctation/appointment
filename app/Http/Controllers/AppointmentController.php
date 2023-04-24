aae8098a257ed4e8ed812639005b5490b97c7c8a
xeroctation <umidakosimova.uk@gmail.com>
2023-04-24 13:36:59 +0500

    code refactor

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,86 +5,50 @@ namespace App\Http\Controllers;
 use App\Mail\AppointmentNotification;
 use App\Models\Appointment;
 use App\Models\ServiceProvider;
+use App\Services\AppointmentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 
 class AppointmentController extends Controller
 {
+    public function __construct(AppointmentService $service)
+    {
+        $this->service = $service;
+    }
+
     public function index()
     {
-        $events = [];
-
-        $appointments = Appointment::where('user_id', Auth::id())->get();
-
-        foreach ($appointments as $appointment) {
-            $events[] = [
-                'id' => $appointment->id,
-                'title' => $appointment->name .  ' - ' . ($appointment->service_provider_id ? $appointment->serviceProvider->name : ''),
-                'start' => \Carbon\Carbon::parse($appointment->start_time)->format('Y-m-d H:i:s'),
-                'end' => \Carbon\Carbon::parse($appointment->end_time)->format('Y-m-d H:i:s'),
-            ];
-        }
+        /** @var object $user*/
+        $user = auth()->user();
+        $events = $this->service->index($user);
 
         return view('appointments.calendar', compact('events'));
     }
 
     public function store(Request $request)
     {
-        $userId = Auth::id();
-
-        $request->validate([
-            'name' => 'required|string'
-        ]);
-
-        $event = Appointment::create([
-            'name' => $request->name,
-            'start_time' => $request->start_time,
-            'end_time' => $request->end_time,
-            'user_id' => $userId,
-            'service_provider_id' => $request->service_provider_id,
-        ]);
-
-        Mail::to($event->user->email)->send(new AppointmentNotification($event, 'created', $request->start_time, $request->end_time));
+        /** @var object $user*/
+        $user = auth()->user();
+        $event = $this->service->store($request, $user);
 
         return response()->json($event);
+    }
+
+    public function update(Request $request ,$id)
+    {
+        $event = $this->service->update($request, $id);
+        return response()->json($event);
+    }
+
+    public function delete(Request $request, $id)
+    {
+        return $this->service->delete($request, $id);
     }
 
     public function serviceProvider()
     {
         $service_providers = ServiceProvider::all();
         return response()->json($service_providers);
-    }
-
-
-
-    public function update(Request $request ,$id)
-    {
-        $event = Appointment::find($id);
-        if(! $event) {
-            return response()->json([
-                'error' => 'Unable to locate the event'
-            ], 404);
-        }
-        $event->update([
-            'start_time' => $request->start_time,
-            'end_time' => $request->end_time,
-        ]);
-        return response()->json('Event updated');
-    }
-
-    public function delete(Request $request, $id)
-    {
-        $event = Appointment::find($id);
-        if(! $event) {
-            return response()->json([
-                'error' => 'Unable to locate the event'
-            ], 404);
-        }
-        $event->delete();
-
-        Mail::to($event->user->email)->send(new AppointmentNotification($event, 'deleted', $request->start_time, $request->end_time));
-
-        return $id;
     }
 }
--- app/Services/AppointmentService.php
+++ app/Services/AppointmentService.php
@@ -1,8 +1,79 @@
 <?php
 
+namespace App\Services;
+
+use App\Mail\AppointmentNotification;
+use App\Models\Appointment;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
 class AppointmentService
 {
-    public function appointment(){
+    public function index(object $user){
+        $events = [];
 
+        $appointments = Appointment::where('user_id', $user->id)->get();
+
+        foreach ($appointments as $appointment) {
+            $events[] = [
+                'id' => $appointment->id,
+                'title' => $appointment->name .  ' - ' . ($appointment->service_provider_id ? $appointment->serviceProvider->name : ''),
+                'start' => \Carbon\Carbon::parse($appointment->start_time)->format('Y-m-d H:i:s'),
+                'end' => \Carbon\Carbon::parse($appointment->end_time)->format('Y-m-d H:i:s'),
+            ];
+        }
+
+        return $events;
     }
+
+    public function store(object $request, object $user)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        $event = Appointment::create([
+            'name' => $request->name,
+            'start_time' => $request->start_time,
+            'end_time' => $request->end_time,
+            'user_id' => $user->id,
+            'service_provider_id' => $request->service_provider_id,
+        ]);
+
+        Mail::to($event->user->email)->send(new AppointmentNotification($event, 'created', $request->start_time, $request->end_time));
+
+        return $event;
+    }
+
+    public function update(object $request, int $id)
+    {
+        $event = Appointment::find($id);
+        if(! $event) {
+            return response()->json([
+                'error' => 'Unable to locate the event'
+            ], 404);
+        }
+        $event->update([
+            'start_time' => $request->start_time,
+            'end_time' => $request->end_time,
+        ]);
+        return 'Event updated';
+    }
+
+    public function delete(object $request, int $id)
+    {
+        $event = Appointment::find($id);
+        if(! $event) {
+            return response()->json([
+                'error' => 'Unable to locate the event'
+            ], 404);
+        }
+        $event->delete();
+
+        Mail::to($event->user->email)->send(new AppointmentNotification($event, 'deleted', $request->start_time, $request->end_time));
+
+        return $id;
+    }
+
+
 }
